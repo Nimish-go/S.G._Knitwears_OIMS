a55afcb3255a016796849f25505d2cd4951c8223
Nimish-go <nimishgodbole409@gmail.com>
2026-06-22 10:56:14 +0530

    feat - Add Recipts Entity

---
+++ backend/src/main/java/com/sg/knitwears/entity/Receipts.java
@@ -0,0 +1,50 @@
+package com.sg.knitwears.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+
+@Entity
+public class Receipts {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@OneToMany
+	@JoinColumn(name="id")
+	private Orders order;
+	
+	@OneToOne
+	@JoinColumn(name = "id")
+	private Users user;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public Orders getOrder() {
+		return order;
+	}
+
+	public void setOrder(Orders order) {
+		this.order = order;
+	}
+
+	public Users getUser() {
+		return user;
+	}
+
+	public void setUser(Users user) {
+		this.user = user;
+	}
+	
+}
